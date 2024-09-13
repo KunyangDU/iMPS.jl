3f7ee8006349512cec736d17492dd50f68290e97
AntiUtopiaPlan <duky2003@163.com>
2024-09-13 17:33:29 +0800

    dky

--- analysis/Transverse Ising/spectrum function/main.jl
+++ analysis/Transverse Ising/spectrum function/main.jl
@@ -5,11 +5,12 @@ include("../model.jl")
 
 J = 1.0
 h = 1.0
-L = 12
+L = 11
+D_MPS = 2^3
 
 lsω = -1.0:0.2:5.0
 
-width,height = 1 .* (300,200)
+width,height = 0.7 .* (600,200)
 
 xtickvalues = [0,pi/2,pi,3*pi/2,2*pi]
 xticklabels = [L"0",L"\pi/2",L"\pi",L"3\pi/2",L"2\pi"]
@@ -19,7 +20,8 @@ ax = Axis(fig[1,1],
 ylabel = L"\omega/t",
 xlabel = L"k",
 xticks = (xtickvalues,xticklabels),
-title = "L=$(L) TransIsing: D=$(D_MPS)",
+title = "TransIsing: L=$(L),J=$(J),h=$(h)D=$(D_MPS)",
+titlealign = :left,
 yticks = -4:2:4,
 width = width,height = height)
 
@@ -34,3 +36,4 @@ end
 
 resize_to_layout!(fig)
 display(fig)
+save("Transverse Ising/figures/Skω_D=$(D_MPS)_L=$(L)_J=$(J).pdf",fig)
